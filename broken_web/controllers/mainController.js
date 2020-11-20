38e8b99f0acae8078d168540321f65c48086f2cd
Gaby <gabrieln1999@gmail.com>
2020-11-20 00:38:03 -0300

    CRUD v1

--- broken_web/controllers/mainController.js
+++ broken_web/controllers/mainController.js
@@ -1,11 +1,19 @@
+const db = require('../database/models');
+
 const mainController = {
   index: function(req, res, next) {
-      res.render('index', { title: 'Broken Youth' });
-  },
-
-  
-  
-
+    db.Productos.findAll({
+      include: [{association: 'imagen'}],
+      raw: true,
+      nest: true
+    })        
+  .then(function(productos){                     
+      res.render("index",{productos});                
+    })
+  .catch(function(error){
+      console.log(error);
+    })
+  } 
 }
 
 module.exports = mainController;
