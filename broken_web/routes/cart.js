d1b17321f81ee3409053a3187a58c39ec52815f7
Juan <juanchipicabohm2992@gmail.com>
2020-12-17 20:29:01 -0300

    react

--- broken_web/routes/cart.js
+++ broken_web/routes/cart.js
@@ -3,7 +3,7 @@ var router = express.Router();
 //const productController = require('../controllers/productController');
 const cartController = require("../controllers/cartController");
 
-//router.get('/carrito', productController.vistaCarrito);
+//router.get('/carrito', cartController.vistaCarrito);
 //router.get("/vistaProductos", productController.vistaProductos); 
 router.post('/detalleProducto/:id', cartController.agregarProducto);
 
